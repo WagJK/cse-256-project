f158ef0cbba2f64eaf73bc1b1ee63695fb054e47
WagJK <wagjk@ucsd.edu>
2019-11-30 10:17:55 -0800

    text selection and display

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.storage.sync.set({color: '#3aa757'}, function() {
-		console.log('The color is green.');
-	});
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {hostEquals: 'developer.chrome.com'},
-			})
-			],
-					actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
-	});
-});
+// create context menu
+var contextMenuItem = {
+	"id": "sentimentAnalysis",
+	"title": "Perform Sentiment Analysis",
+	"contexts": ["selection"]
+}
+chrome.contextMenus.create(contextMenuItem)
+
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+	if (clickData.menuItemId == "sentimentAnalysis" && clickData.selectionText) {
+		chrome.storage.sync.set({'text': clickData.selectionText}, null);
+	}
+})
